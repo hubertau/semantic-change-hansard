4d3f427a75cdabcd5c2ce0216b7a812466bf6e4e
Hubert Au <hubertau@tutanota.com>
2022-11-08 11:29:07 +0000

    full init

--- src/semchange/master_table.py
+++ src/semchange/master_table.py
@@ -9,9 +9,40 @@ import click
 
 @click.command()
 @click.option('--scan_dir', required=True, help='directory within which to scan')
-@click.option('--')
-def main():
-    pass
+@click.option('--outfile', '-o', required=True, help='where to save the master table')
+def main(scan_dir, outfile):
+    files_logreg = glob.glob(os.path.join(scan_dir, '*', '*logreg.csv'), recursive=True)
+    files_nn     = glob.glob(os.path.join(scan_dir, '*', '*nn_comparison.csv'), recursive=True)
+    rows = []
+    for f in files_logreg + files_nn:
+        split_path = os.path.split(f)
+        if 'logreg' in split_path[-1]:
+            model_type = 'logreg'
+        else:
+            model_type = 'nn'
+        country, lang, event = split_path[-2].split('_')
+        data = pd.read_csv(f)
+        model = data['Model'].iat[0]
+        accuracy = data['Accuracy'].iat[0]
+        precision = data['Precision'].iat[0]
+        recall = data['Recall'].iat[0]
+        f1_score = data['F1 Score'].iat[0]
+
+        rows.append({
+            "Model": model,
+            "Type": model_type,
+            "Accuracy": accuracy,
+            "Precision": precision,
+            "Recall": recall,
+            "F1": f1_score,
+            "Country": country,
+            "Language": lang,
+            "Event": event
+        })
+
+    mastertable = pd.DataFrame.from_records(rows)
+    mastertable.to_csv(outfile)
+
 
 if __name__ == '__main__':
     main()

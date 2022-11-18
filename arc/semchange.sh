cc147b7c95b28fc35c0332b5038b30abb9c7f219
Hubert Au <hubertau@tutanota.com>
2022-11-18 17:20:58 +0000

    stuff

--- arc/semchange.sh
+++ arc/semchange.sh
@@ -19,11 +19,11 @@ cd $HOME/2022-08a-Semantic_Change/src/semchange/
 country=$1
 lang=$2
 event="911"
-model="speaker"
+model=$3
 split_date="2001-09-11"
 
-echo $lang
 echo $country
+echo $lang
 echo $event
 echo $model
 echo $split_date
@@ -33,8 +33,14 @@ python semchange.py \
   -c $DATA/data_semantic/02_intermediate/change_${event}_${lang}.txt \
   -nc $DATA/data_semantic/02_intermediate/nochange_${lang}.txt \
   --split_date ${split_date} \
+  --split_range 3 \
   --model ${model} \
-  --retrofit_outdir $DATA/data_semantic/02_intermediate/${lang}_${country}_${event}/ \
-  --tokenized_outdir $DATA/data_semantic/02_intermediate/${lang}_${country}_${event}/ \
-  --outdir $DATA/data_semantic/04_models/${model}/${lang}_${country}_${event}/ \
-  --model_output_dir $DATA/data_semantic/05_model_outputs/${model}/${lang}_${country}_${event}/ \
+  --retrofit_outdir $DATA/data_semantic/02_intermediate/${country}_${lang}_${event}/ \
+  --min_vocab_size 5000 \
+  --tokenized_outdir $DATA/data_semantic/02_intermediate/${country}_${lang}_${event}/ \
+  --outdir $DATA/data_semantic/04_models/${model}/${country}_${lang}_${event}/ \
+  --model_output_dir $DATA/data_semantic/05_model_outputs/${model}/${country}_${lang}_${event}/ \
+  --overwrite_preprocess \
+  --overwrite_postprocess \
+  --overwrite_model \
+  # --log_level DEBUG \
